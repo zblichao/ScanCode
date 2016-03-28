4bc559939a0ad99fe06b026bd0bd8bf6f58890a4
zblichao86 <zblichao86@126.com>
2016-03-28 18:00:47 +0800

    11

--- app/src/main/java/com/lichao/scancode/activity/OutOrderDetialActivity.java
+++ app/src/main/java/com/lichao/scancode/activity/OutOrderDetialActivity.java
@@ -235,7 +235,6 @@ public class OutOrderDetialActivity extends BaseActivity {
         public void onReceiveBarcode(String type, String barcodeStr) {
             ArrayList<NameValuePair> list;
             EditText editText;
-            ToastUtil.showShortToast(getApplicationContext(),barcodeStr);
             switch (type) {
                 case "code128-P":
                     OutOrderDetialActivity.this.barcodeStr = barcodeStr;
