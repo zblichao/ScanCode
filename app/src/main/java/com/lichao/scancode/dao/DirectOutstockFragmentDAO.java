835d149f1ebb8e7ef3a4cd28fe1374a0e1ec2485
zblichao86 <zblichao86@126.com>
2016-04-07 15:47:56 +0800

    11

--- app/src/main/java/com/lichao/scancode/dao/DirectOutstockFragmentDAO.java
+++ app/src/main/java/com/lichao/scancode/dao/DirectOutstockFragmentDAO.java
@@ -34,19 +34,15 @@ public class DirectOutstockFragmentDAO extends CommonDAO {
         return res;
     }
 
-    public String outOrders(String orderId, String customerId, String qty, String productId, String lot, String originStock, String detRowId, String expire, String warehouseId) {
+    public String outOrders(String department, String product_id, String qty,   String LOT, String expire,    String warehouseId) {
         List<NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add(new NameValuePair("action", "deliver"));
-        params.add(new NameValuePair("order_id", orderId));
-        params.add(new NameValuePair("customer_id", customerId));
-        params.add(new NameValuePair("qty_0_0", qty));
-        params.add(new NameValuePair("product_id_0_0", productId));
-        params.add(new NameValuePair("LOT_0_0", lot));
-        params.add(new NameValuePair("origin_stock_0_0", originStock));
-        params.add(new NameValuePair("det_rowid_0_0", detRowId));
-        params.add(new NameValuePair("expire_0_0", expire));
-        params.add(new NameValuePair("warehouse_id_0_0", warehouseId));
-        params.add(new NameValuePair("mobile", "1"));
+        params.add(new NameValuePair("action", "out_dispatch"));
+        params.add(new NameValuePair("department", department));
+        params.add(new NameValuePair("product_id", product_id));
+        params.add(new NameValuePair("qty", qty));
+        params.add(new NameValuePair("LOT", LOT));
+        params.add(new NameValuePair("expire", expire));
+        params.add(new NameValuePair("warehouse_id", warehouseId));
         String res = "";
         try {
             res = HttpUtil.Post("index.php", params);
